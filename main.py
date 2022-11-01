180e358348d96739e6575e3bb4ddf0121c84bc7b
elektritisch <ninagalayko@gmail.com>
2022-11-01 16:33:08 +0300

    add buttons

--- main.py
+++ main.py
@@ -28,20 +28,46 @@ def telegram_bot(token):
 
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        bot.send_message(message.chat.id, 'Привет! Я знаю актуальные цены на Apple Watch. Какой серии часы интересуют? 7/8?')
+        bot.send_message(message.chat.id, f'Привет, {message.chat.first_name}! Я могу показать цены на технику Apple')
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        btn_iphone = telebot.types.KeyboardButton('iPhone')
+        btn_airpods = telebot.types.KeyboardButton('AirPods')
+        btn_airtag = telebot.types.KeyboardButton('AirTag')
+        btn_watch = telebot.types.KeyboardButton('Apple Watch')
+        btn_ipad = telebot.types.KeyboardButton('iPad')
+        btn_macbook = telebot.types.KeyboardButton('Macbook')
+        markup.add(btn_iphone, btn_airpods, btn_airtag, btn_watch, btn_ipad, btn_macbook)
+        bot.send_message(message.chat.id, 'Какая продукция Apple интересует?', reply_markup=markup)
 
     @bot.message_handler(content_types=['text'])
     def send_text(message):
-        if message.text in '78':
+        if message.text == 'iPhone':
+            bot.send_message(message.chat.id, 'Функционал недоступен')
+        elif message.text == 'AirPods':
+            bot.send_message(message.chat.id, 'Функционал недоступен')
+        elif message.text == 'AirTag':
+            bot.send_message(message.chat.id, 'Функционал недоступен')
+        elif message.text == 'Apple Watch':
+            markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            btn7 = telebot.types.KeyboardButton('7')
+            btn8 = telebot.types.KeyboardButton('8')
+            markup.add(btn7, btn8)
+            bot.send_message(message.chat.id, 'Какой серии часы интересуют?', reply_markup=markup)
+        elif message.text == 'iPad':
+            bot.send_message(message.chat.id, 'Функционал недоступен')
+        elif message.text == 'Macbook':
+            bot.send_message(message.chat.id, 'Функционал недоступен')
+
+        elif message.text in '78':
             try:
                 for i in get_data('https://aj.ru/watch.html', type=message.text).items():
                     bot.send_message(message.chat.id, f'{i[0]} {i[1]}')
-                bot.send_message(message.chat.id, 'Чтобы узнать цены на часы другой серии, нажми /start')
+                bot.send_message(message.chat.id, 'Чтобы узнать стоимость другой продукции, нажми /start')
             except Exception as ex:
                 print(ex)
                 bot.send_message(message.chat.id, 'Что-то не так')
         else:
-            bot.send_message(message.chat.id, 'Ок, тогда пока.\nЧтобы запустить меня заново, нажми /start')
+            bot.send_message(message.chat.id, 'Чтобы запустить меня заново, нажми /start')
 
     bot.polling()
 
